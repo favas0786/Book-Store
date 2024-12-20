1a09abc83834597002dcedf00fd228d63501f1bb
Muhammed Favas <favas78601@gmail.com>
2024-12-20 22:17:27 +0530

    complete responsive nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ export default {
     extend: {
       colors:{
         'primery':'#FFCE1A',
+        'secondary':'#0d0842',
+        'blackBG':'#f3f3f3',
+        'Favorite':'#ff5841'
+      },
+      fontFamily:{
+        'primery':["Montserrat", "sans-serif"],
+        'secondary':["Nunito Sans", "sans-serif"]
       }
     },
   },
